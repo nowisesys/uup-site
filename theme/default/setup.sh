ebe30f8c63dcd8ce8d05abf709c71923041f7c09
Anders Lövgren <andlov@nowise.se>
2016-10-17 17:18:09 +0000

    Work in progress (default theme).

--- theme/default/setup.sh
+++ theme/default/setup.sh
@@ -5,22 +5,18 @@
 
 set -x
 
-name=bootstrap
+name=default
 destdir=${destdir:-"public"}
 
 cwd=$(pwd)
 src=$(realpath $(dirname $0))
 
-mkdir -p $destdir/theme/$name/assets/{css,fonts,js,img}
+mkdir -p $destdir/theme/$name/assets/{css,fonts,img}
 mkdir -p $cwd/template/$name
 
-ln -sf $cwd/vendor/twitter/bootstrap/dist/css/*.min.css $destdir/theme/$name/assets/css
-ln -sf $cwd/vendor/twitter/bootstrap/dist/fonts/*       $destdir/theme/$name/assets/fonts
-ln -sf $cwd/vendor/twitter/bootstrap/dist/js/*.min.js   $destdir/theme/$name/assets/js
-ln -sf $cwd/vendor/frameworks/jquery/jquery.min.js      $destdir/theme/$name/assets/js
-
 cp -a $src/public/css/* $destdir/theme/$name/assets/css
-cp -a $src/public/img/* $destdir/theme/$name/assets/img
+ln -sf $src/public/img/* $destdir/theme/$name/assets/img
+ln -sf $src/public/fonts/* $destdir/theme/$name/assets/fonts
 
 for file in $src/template/*; do
     if ! [ -e $cwd/template/$file ]; then
