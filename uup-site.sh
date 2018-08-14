4af57156ef8d3e2128a5aaf07befc4edbf912718
Anders Lövgren <andlov@nowise.se>
2018-08-14 15:34:46 +0000

    Adding note about leading and trailing slashes being required.

--- uup-site.sh
+++ uup-site.sh
@@ -265,6 +265,7 @@ function usage()
     echo
     echo "Notice:"
     echo "  1. The --location or --verbose options must be used before any other option."
+    echo "  2. All location pathes must have leading and trailing slashes ('/')." 
     echo 
     echo "Copyright (C) 2015-2018 Nowise Systems and Uppsala University (Anders Lövgren, BMC-IT)"
 }
